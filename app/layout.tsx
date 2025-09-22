09aa28492d6c3d21051efa9f17c51c71ba2ce90c
feng <rannuofli@gmail.com>
2025-09-22 21:37:54 +0200

    homepage and signin signout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
+import { frFR } from "@clerk/localizations";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,15 +26,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        > 
-        
-            <Navbar />
-            {children}
-          
+    <ClerkProvider   localization={frFR}>
+      <html lang="fr">
+        <body >
+          <Navbar />
+          <div  className="max-w-7xl mx-auto pt-16 p-4 min-h-screen">{children}</div>
           
         </body>
       </html>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 
-import { SignedIn, SignedOut, useUser } from '@clerk/nextjs';
+import { SignedIn, SignedOut, useUser, SignOutButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+
 
 export default function Navbar(){
   const {isLoaded, isSignedIn, user } = useUser();
@@ -12,30 +12,42 @@ export default function Navbar(){
 
   return(
     <>
-      <nav>
-        {""}
-        <div>
+      <nav className='navbar-global'>
+      
+        <div className='navbar-items'>
           <Link href="/">
-            <Image src="/logo.png" width={60}  height={60} alt="logo" />
+            <Image src="/logo.png" width={60}  height={60} alt="logo" className='navbar-logo' />
           </Link>     
+        
+          <div className='space-x-6 flex items-center'>
+            <SignedIn>
+             
+              {user?.imageUrl ? (<Link href="/profile"> 
+              <Image src={user.imageUrl} alt="profile picture" width={40} height={40} className='rounded-full'/>
+              </Link>)
+              : (<div className='w-10 h-10 bg-gray-300 rounded-full'></div>)}
+
+              <Link
+                  href="/mealplan" className='navbar-link'
+                >
+                Mealplan
+              </Link>
+              <Link href="/subscribe" className='navbar-link'>Subscribe</Link>
+
+              <SignOutButton>
+                <button className='navbar-signoutbutton  bg-emerald-500 hover:bg-emerald-600 transition'>Sign out</button>
+              </SignOutButton>
+            </SignedIn>
+
+            <SignedOut>
+              <Link href="/" className='avbar-link'>Home</Link>
+              <Link href={isSignedIn ? "/subscribe" : "/sign-up"} className='avbar-link'>Subscribe</Link>
+              <Link href="/sign-up" className='navbar-signoutbutton  bg-emerald-500 hover:bg-emerald-600 transition'>Sign up</Link>
+              
+            </SignedOut>
+          </div>
         </div>
 
-        <div>
-          <SignedIn>
-            <Link
-                href="/mealplan"
-              >
-              Mealplan
-            </Link>
-            {user?.imageUrl ? (<Link href="/profile"> 
-            <Image src={user.imageUrl} alt="profile picture" width={40} height={40}/>
-            </Link>)
-             : (<div></div>)}
-          </SignedIn>
-          <SignedOut>
-
-          </SignedOut>
-        </div>
       </nav>
     </>
   );
