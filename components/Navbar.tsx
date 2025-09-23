c81c889cceac969bfdaaab0aacbc36843c873255
feng <rannuofli@gmail.com>
2025-09-23 21:32:44 +0200

    change navbar to add model fix problem to jump to sign up page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 
-import { SignedIn, SignedOut, useUser, SignOutButton } from '@clerk/nextjs';
+import { SignedIn, SignedOut, useUser, SignOutButton, SignUp } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 
 export default function Navbar(){
   const {isLoaded, isSignedIn, user } = useUser();
+  const [open, setOpen] = useState(false);
   if (!isLoaded) <p>Loading ...... </p>;
+
+  useEffect(() => {
+    if (isSignedIn) setOpen(false);
+  }, [isSignedIn]);
+
+
 
   return(
     <>
@@ -20,7 +28,7 @@ export default function Navbar(){
           </Link>     
         
           <div className='space-x-6 flex items-center'>
-            <SignedIn>
+            <SignedIn >
              
               {user?.imageUrl ? (<Link href="/profile"> 
               <Image src={user.imageUrl} alt="profile picture" width={40} height={40} className='rounded-full'/>
@@ -34,7 +42,7 @@ export default function Navbar(){
               </Link>
               <Link href="/subscribe" className='navbar-link'>Subscribe</Link>
 
-              <SignOutButton>
+              <SignOutButton redirectUrl={typeof window !== "undefined" ? window.location.href : "/"}>
                 <button className='navbar-signoutbutton  bg-emerald-500 hover:bg-emerald-600 transition'>Sign out</button>
               </SignOutButton>
             </SignedIn>
@@ -42,13 +50,34 @@ export default function Navbar(){
             <SignedOut>
               <Link href="/" className='avbar-link'>Home</Link>
               <Link href={isSignedIn ? "/subscribe" : "/sign-up"} className='avbar-link'>Subscribe</Link>
-              <Link href="/sign-up" className='navbar-signoutbutton  bg-emerald-500 hover:bg-emerald-600 transition'>Sign up</Link>
+              <button
+                onClick={() => setOpen(true)}
+                className="navbar-signoutbutton bg-emerald-500 hover:bg-emerald-600 transition"
+              >
+                Sign up
+              </button>
               
             </SignedOut>
           </div>
         </div>
 
       </nav>
+      
+      {open && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+          <div className="bg-white p-6 rounded-2xl shadow-lg max-w-md w-full relative">
+            <button
+              onClick={() => setOpen(false)}
+              className="absolute top-2 right-2 text-gray-500"
+            >
+              ✕
+            </button>
+
+            <SignUp routing="virtual" signInUrl="#" redirectUrl={typeof window !== "undefined" ? window.location.href : "/"} />
+          </div>
+        </div>
+      )}
+
     </>
   );
 }
