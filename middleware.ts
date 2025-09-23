d76fa1ae865916ae9f969d5be20d6535b1fa6a0a
feng <rannuofli@gmail.com>
2025-09-23 12:05:33 +0200

    set up middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,37 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-up(.*)",
+  "/subscribe(.*)",
+
+]);
+
+const isSignUpRoute = createRouteMatcher(["/sign-up(.*)"]);
 
 
 // Clerk's middleware
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  const userAuth = await auth();
+  const { userId } = userAuth;
+  const { pathname, origin } = req.nextUrl;
+
+  console.log("middleware info", origin, pathname, userId);
+
+  if (!isPublicRoute(req) && !userId) {
+    return NextResponse.redirect(new URL("/sign-up", origin));
+  }
+
+  // If user is signed in and visits /sign-up → redirect to mealplan
+  if (isSignUpRoute(req) && userId) {
+    return NextResponse.redirect(new URL("/mealplan", origin));
+  }
+
+  return NextResponse.next();
+
+});
 
 export const config = {
   matcher: [
