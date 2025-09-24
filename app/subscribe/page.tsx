4cbac89a9ebcce308be5e7731059842ecd976b62
feng <rannuofli@gmail.com>
2025-09-24 20:14:19 +0200

    basic plan layout in subsribe page

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,9 +1,21 @@
+import SubscriptionCard from '@/components/SubscriptionCard';
 import React from 'react'
 
-const page = () => {
+export default function Subscribe () {
   return (
-    <div>page</div>
-  )
+    <div className="px-4 py-8 sm:py-12 lg:py-16">
+
+      <div>
+        <h2 className="heading">
+          Pricing
+        </h2>
+        <p className="sub-heading">
+          Get started on our weekly plan or upgrade to monthly or yearly when
+          you’re ready.
+        </p>
+        <SubscriptionCard />
+      </div>
+    </div>
+  );
 }
 
-export default page
---
+++ components/SubscriptionCard.tsx
@@ -0,0 +1,68 @@
+import { availablePlans } from "@/lib/plan";
+
+export default function SubscriptionCard(){
+    return (
+        <div className="subscription-container">
+        {availablePlans.map((plan, key) => (
+            <div
+              key={key}
+              className="
+                subscription-option-card
+              "
+            >
+              <div className="flex-1">
+                {/* Conditionally render "Most popular" label */}
+                {plan.isPopular && (
+                  <p className="is-popular">
+                    Most popular
+                  </p>
+                )}
+                <h3 className="text-xl font-semibold">{plan.name}</h3>
+                <p className="mt-4 flex items-baseline">
+                  <span className="text-5xl font-extrabold tracking-tight">
+                    ${plan.amount}
+                  </span>
+                  <span className="ml-1 text-xl font-semibold">
+                    /{plan.interval}
+                  </span>
+                </p>
+                <p className="mt-6">{plan.description}</p>
+                <ul role="list" className="mt-6 space-y-4">
+                  {plan.features.map((feature, index) => (
+                    <li key={index} className="flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="flex-shrink-0 w-6 h-6 text-emerald-500"
+                      >
+                        <polyline points="20 6 9 17 4 12" />
+                      </svg>
+                      <span className="ml-3">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+                </div>
+
+                <button
+                className={`${
+                    plan.interval === "month"
+                    ? "bg-emerald-500 text-white  hover:bg-emerald-600 "
+                    : "bg-emerald-100 text-emerald-700  hover:bg-emerald-200 "
+                }  subscrib-button`}
+                
+                >
+                    Subscribe {plan.name}
+                </button>
+                </div>
+                ))}
+
+        </div>
+    )
+}
